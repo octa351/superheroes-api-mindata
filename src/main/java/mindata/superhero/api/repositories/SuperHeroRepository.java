e0b46b2f19b18b8100289f0db02cf6c6897c3148
octa351 <octaviosanti45@gmail.com>
2022-12-21 19:37:31 -0300

    Added cache for gets

--- src/main/java/mindata/superhero/api/repositories/SuperHeroRepository.java
+++ src/main/java/mindata/superhero/api/repositories/SuperHeroRepository.java
@@ -1,14 +1,21 @@
 package mindata.superhero.api.repositories;
 
 import mindata.superhero.api.models.SuperHero;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SuperHeroRepository extends JpaRepository<SuperHero,Long> {
+    @Cacheable("superHeroesByName")
     @Query("SELECT s FROM SuperHero s WHERE lower(s.name) LIKE lower(concat('%', :sequence,'%'))")
     List<SuperHero> findSuperHeroContainingCharSequence(String sequence);
+
+    @Cacheable("superHeroesById")
+    @Override
+    Optional<SuperHero> findById(Long id);
 }
