24ca8f0394410dbea82c2742aba1af9d4e902ca8
octa351 <octaviosanti45@gmail.com>
2022-12-21 00:22:40 -0300

    Updated error handling

---
+++ src/main/java/mindata/superhero/api/exceptions/ApiError.java
@@ -0,0 +1,17 @@
+package mindata.superhero.api.exceptions;
+
+public class ApiError {
+    String message;
+
+    public ApiError(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/mindata/superhero/api/exceptions/RestResponseEntityExceptionHandler.java
+++ src/main/java/mindata/superhero/api/exceptions/RestResponseEntityExceptionHandler.java
@@ -8,17 +8,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(HeroNotFoundException.class)
     public ResponseEntity<Object> handleHeroNotFound(RuntimeException ex, WebRequest request) {
         String validationMessage = ex.getMessage();
-        return handleExceptionInternal(ex, validationMessage, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+        ApiError errorBody = new ApiError(validationMessage);
+        return handleExceptionInternal(ex, errorBody, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler(InvalidHeroException.class)
     public ResponseEntity<Object> handleInvalidHero(RuntimeException ex, WebRequest request) {
         String validationMessage = ex.getMessage();
-        return handleExceptionInternal(ex, validationMessage, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+        ApiError errorBody = new ApiError(validationMessage);
+        return handleExceptionInternal(ex, errorBody, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 }
